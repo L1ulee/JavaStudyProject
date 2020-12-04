0bc60ac977799bddeb0a3bc394167b79d1439feb
lee <1141123287@qq.com>
2020-12-05 04:41:36 +0800

    项目初始化

---
+++ src/Java_Basic/Day01/HelloWorld.java
@@ -0,0 +1,11 @@
+package Java_Basic.Day01;
+
+public class HelloWorld {                     //创建Helloworld类
+
+    public static void main(String[] args) {  //实现主方法
+
+        System.out.println("Hello,World!");   //输出Hello,World!
+
+    }
+
+}
